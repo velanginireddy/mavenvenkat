4eec349895e48ec5090c3dbcae85f63eae413589
velangini <velanginibhavanam@gmail.com>
2018-08-23 13:59:02 +0530

    pom files

---
+++ src/main/java/velangini/App.java
@@ -0,0 +1,13 @@
+package velangini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
